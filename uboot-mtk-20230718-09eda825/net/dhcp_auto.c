2c079d6de439c0e7b6ca23cb402ab1aec8621a64
sfsk00 <sfsk@foxmail.com>
2025-09-20 21:57:41 +0800

    Update dhcp_auto.c

--- uboot-mtk-20230718-09eda825/net/dhcp_auto.c
+++ uboot-mtk-20230718-09eda825/net/dhcp_auto.c
@@ -1,3 +1,4 @@
+
 #include <common.h>
 #include <command.h>
 #include <net.h>
@@ -13,36 +14,35 @@ static int do_dhcp_autoconfig(struct cmd_tbl *cmdtp, int flag, int argc,
 {
     int ret;
     struct udevice *dev;
-    char ip_str[16];
+    struct in_addr ip_addr;
     
     /* 硬件初始化 */
-    ret = uclass_get_device(UCLASS_ETH, 0, &dev);
-    if (ret) {
-        printf("Ethernet init failed (err=%d)\n", ret);
+    if (uclass_get_device(UCLASS_ETH, 0, &dev)) {
+        printf("Ethernet device not found\n");
         return CMD_RET_FAILURE;
     }
 
-    /* DHCP参数设置 */
+    /* 设置DHCP参数 */
     env_set_ulong("netretry", DHCP_RETRY_MAX);
-    net_dhcp_timeout = DHCP_TIMEOUT_MS;
+    env_set_ulong("timeout", DHCP_TIMEOUT_MS);
 
     /* DHCP主流程 */
     for (int i = 0; i < DHCP_RETRY_MAX; i++) {
         printf("DHCP attempt %d/%d\n", i+1, DHCP_RETRY_MAX);
         ret = net_loop(DHCP);
         
-        if (ret == 0) {
-            ip_to_string(net_ip_addr, ip_str);
-            printf("DHCP success: IP=%s\n", ip_str);
-            env_set_ip("ipaddr", net_ip_addr);
-            env_set_ip("serverip", net_server_ip);
-            return CMD_RET_SUCCESS;
+        if (!ret) {
+            /* 获取并显示分配的IP */
+            if (!net_parse_ipv4(env_get("ipaddr"), &ip_addr)) {
+                printf("DHCP success: IP=%s\n", inet_ntoa(ip_addr));
+                return CMD_RET_SUCCESS;
+            }
         }
         mdelay(100);
     }
 
     /* 失败回退处理 */
-    printf("DHCP failed, using fallback IP\n");
+    printf("Using fallback IP 192.168.1.100\n");
     env_set("ipaddr", "192.168.1.100");
     env_set("netmask", "255.255.255.0");
     return CMD_RET_FAILURE;

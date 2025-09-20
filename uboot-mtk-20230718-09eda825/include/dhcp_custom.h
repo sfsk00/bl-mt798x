59af04a82f52103dd667c11c3ccce9ddfd42eaf7
sfsk00 <sfsk@foxmail.com>
2025-09-20 17:23:52 +0800

    Update dhcp_custom.h

--- uboot-mtk-20230718-09eda825/include/dhcp_custom.h
+++ uboot-mtk-20230718-09eda825/include/dhcp_custom.h
@@ -1,10 +1,10 @@
+
 #ifndef __DHCP_CUSTOM_H__
 #define __DHCP_CUSTOM_H__
 
 #include <command.h>
+#include <net.h>
 
 int do_dhcp_custom(struct cmd_tbl *cmdtp, int flag, int argc, char *const argv[]);
-extern int do_dhcp(struct cmd_tbl *cmdtp, int flag, int argc, char *const argv[]);
-extern void net_set_ip(struct in_addr ip);
 
 #endif

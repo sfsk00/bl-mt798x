772996280508c1294c4c3ab06e968948a616ac01
sfsk00 <sfsk@foxmail.com>
2025-09-20 17:10:27 +0800

    Update dhcp_custom.h

--- uboot-mtk-20230718-09eda825/include/dhcp_custom.h
+++ uboot-mtk-20230718-09eda825/include/dhcp_custom.h
@@ -1,16 +1,10 @@
-
 #ifndef __DHCP_CUSTOM_H__
 #define __DHCP_CUSTOM_H__
 
-struct dhcp_extension {
-    uint8_t client_mac[6];
-    uint32_t requested_ip;
-    uint8_t option_buf[128];
-};
-
-int dhcp_custom_request(void);
-void dhcp_set_fallback_ip(uint32_t ip);
+#include <command.h>
 
 int do_dhcp_custom(struct cmd_tbl *cmdtp, int flag, int argc, char *const argv[]);
+extern int do_dhcp(struct cmd_tbl *cmdtp, int flag, int argc, char *const argv[]);
+extern void net_set_ip(struct in_addr ip);
 
 #endif

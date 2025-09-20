af7617cf7ba40977a3dc0732f1efd824e5612da2
sfsk00 <sfsk@foxmail.com>
2025-09-20 13:19:31 +0800

    Add files via upload

---
+++ uboot-mtk-20230718-09eda825/include/dhcpd.h
@@ -0,0 +1,24 @@
+
+#ifndef __DHCPD_H__
+#define __DHCPD_H__
+
+struct dhcp_packet {
+    uint8_t op;
+    uint8_t htype;
+    uint8_t hlen;
+    uint8_t hops;
+    uint32_t xid;
+    uint16_t secs;
+    uint16_t flags;
+    uint32_t ciaddr;
+    uint32_t yiaddr;
+    uint32_t siaddr;
+    uint32_t giaddr;
+    uint8_t chaddr[16];
+    char sname[64];
+    char file[128];
+    uint32_t cookie;
+    uint8_t options[312];
+};
+
+#endif

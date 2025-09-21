93e65fe4e631c82cc4b4ae5e85671396b53fdcdd
sfsk00 <sfsk@foxmail.com>
2025-09-21 17:14:53 +0800

    Update mt7981.h

--- uboot-mtk-20230718-09eda825/include/configs/mt7981.h
+++ uboot-mtk-20230718-09eda825/include/configs/mt7981.h
@@ -9,5 +9,11 @@
 #ifndef __MT7981_H
 #define __MT7981_H
 
+/* 添加BusyBox支持 */
+#define CONFIG_CMD_BUSYBOX
+#define CONFIG_BUSYBOX_PATH "/bin/busybox"
+#define CONFIG_BUSYBOX_CMDLIST \
+    "ls", "cat", "echo", "mkdir", "rm", "cp", "mv"
+
 
 #endif

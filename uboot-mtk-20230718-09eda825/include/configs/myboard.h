9b5905aa7ee4d3115eb93cfa18de0305ddcf3ded
sfsk00 <sfsk@foxmail.com>
2025-09-21 12:08:59 +0800

    Delete uboot-mtk-20230718-09eda825/include/configs/myboard.h

--- uboot-mtk-20230718-09eda825/include/configs/myboard.h
+++
@@ -1,5 +0,0 @@
-
-#include <configs/dhcp_ip.h>
-#define CONFIG_BOOTCOMMAND "run netboot"
-#define CONFIG_EXTRA_ENV_SETTINGS \
-    "netboot=dhcp_with_fallback; tftp ${loadaddr} ${bootfile}; bootm\0"
